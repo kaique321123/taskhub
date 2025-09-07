5242a63130f5d40ea0973024b9866f4ff105d8fd
Kaique de Jesus Pessoa Santos <kaique10defevereiro@gmail.com>
2025-09-07 19:20:38 -0300

    Arrumando o home_spec, estava com o endereço errado

--- spec/requests/home_spec.rb
+++ spec/requests/home_spec.rb
@@ -1,11 +1,12 @@
+# spec/requests/home_spec.rb (CORRIGIDO)
+
 require 'rails_helper'
 
 RSpec.describe "Homes", type: :request do
-  describe "GET /index" do
+  describe "GET /" do # Você pode opcionalmente mudar a descrição aqui também
     it "returns http success" do
-      get "/home/index"
+      get "/"  # <-- CORREÇÃO
       expect(response).to have_http_status(:success)
     end
   end
-
 end
